09a04222f376c20b11bb2934dbf0889e117b53ea
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-04-19 11:14:24 +0200

    ok

--- ubung/ue3/grammar/code-nabil/scratch.py
+++ ubung/ue3/grammar/code-nabil/scratch.py
@@ -8,6 +8,9 @@ with open("../data/telescope.srgs", "r") as f:
     gr = grammar.Grammar(lines)
 print(gr)
 
+# check if it is CNF
+print(gr.is_CNF())
+
 print(parser.example_telescope_parse().to_dot())
 
 sentence = "I saw the duck with a telescope"
@@ -15,6 +18,6 @@ tokens = sentence.split(" ")
 
 parser.is_in_language(tokens, gr)
 
-parsing_results = parse(tokens, gr)  # oups this fails at the moment...
+# parsing_results = parse(tokens, gr)  # ups this fails at the moment...
 # one of the parsing results should yield the same result as the example from above
-assert repr(parser.example_telescope_parse()) in map(repr, parsing_results)
+# assert repr(parser.example_telescope_parse()) in map(repr, parsing_results)
